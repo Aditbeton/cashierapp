971baed0637efd8e5dfa10d3306ac965f99aa2dd
Aditbeton <142952102+Aditbeton@users.noreply.github.com>
2026-02-04 11:34:24 +0700

    Add files via upload

---
+++ storage/framework/views/fff74214e5840769dbd6bb4ff6f36a9b.php
@@ -0,0 +1,3 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 1945 <a href="/"><?php echo e(config('app.name')); ?></a>.</strong> All rights reserved.
+</footer><?php /**PATH C:\laragon\www\mugon-tokas\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
